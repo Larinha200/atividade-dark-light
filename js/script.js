2b9d2c4d085462ac78a56df8be0f8a5c0580305a
Larinha200 <laravitoriaqueiroz4@gmail.com>
2026-09-22 13:01:16 -0300

    fim

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
+ // Aguarda a página carregar
+        document.addEventListener('DOMContentLoaded', () => {
+            const themeBtn = document.getElementById('themeBtn');
+            const htmlElement = document.documentElement;
 
-document.addEventListener('DOMContentLoaded', () => {
-    const themeBtn = document.getElementById('themeBtn');
-    const htmlElement = document.documentElement;
-
-    themeBtn.addEventListener('click', () => {
-        // Alterna a classe 'dark' no HTML
-        htmlElement.classList.toggle('dark');
-        
-        // Verifica se ficou no dark mode ou light mode
-        const isDarkMode = htmlElement.classList.contains('dark');
-        
-        // Altera o atributo data-theme para light ou dark
-        htmlElement.setAttribute('data-theme', isDarkMode ? 'dark' : 'light');
-        
-        // Altera o texto do botão de acordo com o modo atual
-        themeBtn.textContent = isDarkMode ? 'Light mode' : 'Dark mode';
-    });
-});
+            themeBtn.addEventListener('click', () => {
+                // Alterna a classe 'dark' no HTML
+                htmlElement.classList.toggle('dark');
+                
+                // Verifica se ficou no dark mode ou light mode
+                const isDarkMode = htmlElement.classList.contains('dark');
+                
+                // Altera o atributo data-theme para light ou dark
+                htmlElement.setAttribute('data-theme', isDarkMode ? 'dark' : 'light');
+                
+                // Altera o texto do botão de acordo com o modo atual
+                themeBtn.textContent = isDarkMode ? 'Light mode' : 'Dark mode';
+            });
+        });
